64f9cd4eb139f53107e92eaa0a8b4be685183074
hagee <hagee.ktr@outlook.com>
2025-03-14 16:24:29 +0300

    Create event listener function

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 function addingEventListener() {
+    const input = document.getElementById('button');
+    input.addEventListener('click', clickAlert);
+    function clickAlert () {
+        alert('I was clicked!')
+    }
 }
+
+addingEventListener();
